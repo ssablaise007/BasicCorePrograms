2002f5cd759ad959fbc500dd7bbc22db788edc47
ssablaise007 <75985793+ssablaise007@users.noreply.github.com>
2021-06-30 18:26:45 +0530

    Sec_1_triplets

---
+++ Sec_1.java
@@ -0,0 +1,14 @@
+package BasicCorePrograms;
+import Utility.java.Utility;
+public class Sec_1 {
+
+	public static void main(String[] args) {
+		Utility utility = new Utility();
+		System.out.println("Enter t");
+		int N = utility.getIntValue();
+		int[] array = utility.getArray(N);
+		
+		
+		
+	}
+}
